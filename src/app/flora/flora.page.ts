c48e44aeb926bb1a73902200f249464f3b72b97c
SEDETUS <sistemassedetus@gmail.com>
2019-11-28 23:08:24 -0500

    Añadiendo filtro de busqueda

--- src/app/flora/flora.page.ts
+++ src/app/flora/flora.page.ts
@@ -13,6 +13,7 @@ export class FloraPage implements OnInit {
   
 
   items: Array<any>;
+  listItems;
 
   constructor(
     public loadingCtrl: LoadingController,
@@ -27,6 +28,24 @@ export class FloraPage implements OnInit {
       this.getData();
     }
   }
+
+  initializeItems(){ 
+    this.items = this.listItems;
+  }
+  getItems(ev:any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.payload.doc.data().title.toLowerCase().includes(val.toLowerCase()));
+      })
+    }
+    
+  }
   async getData(){
     const loading = await this.loadingCtrl.create({
       message: 'Por favor espere...'
@@ -37,6 +56,7 @@ export class FloraPage implements OnInit {
       routeData['data'].subscribe(data => {
         loading.dismiss();
         this.items = data;
+        this.listItems=data;
       })
     })
   }
